8b85615c3ca3dee1f02a80515a6f7b11c0a32f10
Minna <minnalnh@gmail.com>
2026-09-03 15:48:13 +0200

    fixed yml paths and finished get characters and get characters by id

--- functions/characters/getCharacterById/index.mjs
+++ functions/characters/getCharacterById/index.mjs
@@ -1,5 +1,20 @@
-import { sendResponse } from "../../responses/index.mjs";
+import { sendResponse } from "../../../responses/index.mjs";
+import { characters } from "../../../data/characters.mjs";
 
 export const handler = async (event) => {
-  return sendResponse(200, { message: "It's alive" });
+  console.log("EVENT:", JSON.stringify(event));
+  const { id } = event.pathParameters;
+  const character = characters.find((c) => c.id === Number(id));
+
+  if (character) {
+    return sendResponse(200, {
+      success: true,
+      character,
+    });
+  } else {
+    return sendResponse(404, {
+      success: false,
+      message: "No character with corresponding ID found",
+    });
+  }
 };
--- functions/characters/getCharacters/index.mjs
+++ functions/characters/getCharacters/index.mjs
@@ -1,5 +1,21 @@
-import { sendResponse } from "../../responses/index.mjs";
+import { sendResponse } from "../../../responses/index.mjs";
+import { characters } from "../../../data/characters.mjs";
 
 export const handler = async (event) => {
-  return sendResponse(200, { message: "It's alive" });
+  const house = event.queryStringParameters?.house?.toLowerCase();
+
+  if (house) {
+    const filtered = characters.filter((c) =>
+      c.house?.toLowerCase().includes(house),
+    );
+    return sendResponse(200, {
+      success: true,
+      characters: filtered,
+    });
+  } else {
+    return sendResponse(200, {
+      success: true,
+      characters,
+    });
+  }
 };
